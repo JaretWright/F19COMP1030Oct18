ce9f1a8183570ac8ba4d96ef98d66d6781eadff2
Jaret Wright <hkkA223>
2019-10-18 10:44:45 -0400

    Final updates

--- src/Craps.java
+++ src/Craps.java
@@ -4,7 +4,6 @@ public class Craps {
 
     /**
      * enums will be used to store the status of the game
-     * @param args
      */
     private enum Status {CONTINUE, WON, LOST};
 
@@ -17,21 +16,46 @@ public class Craps {
 
     public static void main(String[] args)
     {
+        Status gameStatus;
+
         int sumOfDice = rollTwoDice();
+
+        if (sumOfDice==SEVEN || sumOfDice==YO_LEVEN)
+            gameStatus = Status.WON;
+        else if (sumOfDice==SNAKE_EYES || sumOfDice==TREY || sumOfDice==BOX_CARS)
+            gameStatus = Status.LOST;
+        else
+            gameStatus=Status.CONTINUE;
+
+        int myPoint = sumOfDice;
+
+        while (gameStatus==Status.CONTINUE)
+        {
+            sumOfDice=rollTwoDice();
+
+            if (sumOfDice==myPoint)
+                gameStatus = Status.WON;
+            else if (sumOfDice==SEVEN)
+                gameStatus = Status.LOST;
+        }
+
+        if (gameStatus==Status.WON)
+            System.out.printf("Player wins");
+        else
+            System.out.printf("Player loses - the house ALWAYS WINS!!");
     }
 
     /**
      * This method will simulate rolling 2 dice and return their sum
      */
-    public static int rollTwoDice()
-    {
+    public static int rollTwoDice() {
         SecureRandom rng = new SecureRandom();
 
-        int die1 = rng.nextInt(6)+1;
-        int die2 = rng.nextInt(6)+1;
+        int die1 = rng.nextInt(6) + 1;
+        int die2 = rng.nextInt(6) + 1;
 
         System.out.printf("Die 1: %d        Die 2: %d %n", die1, die2);
 
-        return die1+die2;
+        return die1 + die2;
     }
 }
